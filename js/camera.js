aaa6c69d77a4e3dcb6deb869874835453fa4cb4c
JMF300 <rcboss2020@hotmail.com>
2025-05-07 17:53:49 -0500

    Delete js directory

--- js/camera.js
+++
@@ -1,9 +0,0 @@
-// --- Camera Updates ---
-function updateCamera() {
-    const targetX = playerBody.position.x;
-    const targetY = playerBody.position.y + PLAYER_HEIGHT / 2;
-    const targetZ = playerBody.position.z;
-    cameraTarget.set(targetX, targetY, targetZ);
-    camera.position.lerp(cameraTarget, 0.2);
-    camera.lookAt(playerBody.position);
-}
